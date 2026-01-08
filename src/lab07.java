8ee42dd8c09063db4a2ba4d3afa7effd909fc6a6
shubham namdeo <84262316+shubhamnamdev95@users.noreply.github.com>
2026-01-08 13:26:40 +0530

    Add files via upload

---
+++ src/lab07.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class lab07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to calculator");
+        System.out.println("Enter no. A");
+        System.out.println("Enter no. B");
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+
+
+    }
+}
